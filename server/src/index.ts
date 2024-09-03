02abc583655c3347b1cb2875f99070135f69eb86
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-09-03 18:07:09 +0700

    feat: add swagger and cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,21 @@
+import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Trash Talk Documentation",
+          version: "0.1.0",
+        },
+      },
+    })
+  )
+  .use(cors())
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
@@ -10,5 +25,5 @@ console.log(
   `
   Try this:
   
-  \x1b]8;;${app.server?.hostname}:${app.server?.port}/swagger/\x1b\\${app.server?.hostname}:${app.server?.port}/swagger\x1b]8;;\x1b\\`
+  \x1b]8;;http://${app.server?.hostname}:${app.server?.port}/swagger/\x1b\\${app.server?.hostname}:${app.server?.port}/swagger\x1b]8;;\x1b\\`
 );
