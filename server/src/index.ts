3b30c38ca2d664d3e6326d368fe74a58d19e0386
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-09-03 18:04:02 +0700

    feat: add swagger hyperlink

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,3 +5,10 @@ const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+console.log(
+  `
+  Try this:
+  
+  \x1b]8;;${app.server?.hostname}:${app.server?.port}/swagger/\x1b\\${app.server?.hostname}:${app.server?.port}/swagger\x1b]8;;\x1b\\`
+);
